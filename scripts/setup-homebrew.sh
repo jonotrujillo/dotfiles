6619fe7a0781bb8f5d7b380211697f0b1b60896f
Jonathan Trujillo <git@jonathantrujillo.com>
2024-09-23 13:35:48 +0200

    scripts: remove yarn

--- scripts/setup-homebrew.sh
+++ scripts/setup-homebrew.sh
@@ -6,10 +6,9 @@ brew install fnm
 brew install font-input
 brew install fzf
 brew install git
-brew install logitech-options
+brew install logi-options+
 brew install node
 brew install stow
 brew install vim
-brew install yarn
 brew install z
 brew install zsh
