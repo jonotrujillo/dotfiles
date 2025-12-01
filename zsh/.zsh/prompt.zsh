a53ef99ec2ee797bf1d2c4aad87b2352b427be72
Jonathan Trujillo <git@jonathantrujillo.com>
2025-12-01 23:38:28 +0100

    zsh: update prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,5 +1,17 @@
 setopt PROMPT_SUBST
 
+function git_prompt_info() {
+  local ref
+  ref=$(git symbolic-ref --short -q HEAD 2> /dev/null)
+  if [[ -n "$ref" ]]; then
+    local git_status=""
+    if [[ -n $(git status --porcelain --untracked-files=no 2> /dev/null) ]]; then
+      git_status="*"
+    fi
+    echo "%F{242}at ${ref}${git_status}%f"
+  fi
+}
+
 PROMPT='
-%B%F{blue}%~%f%b %F{cyan}$(git symbolic-ref --short -q HEAD 2> /dev/null)%f
+%B%F{blue}%~%f%b $(git_prompt_info)
 '
