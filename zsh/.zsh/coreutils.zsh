f8c4534701d4c371b705ef5813b071f8c4b35519
Jonathan Trujillo <git@jonathantrujillo.com>
2026-04-06 21:16:59 +0200

    brew: add developer tools

--- zsh/.zsh/coreutils.zsh
+++
@@ -1,5 +0,0 @@
-export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
-
-if [[ -x "$(command -v gls)" ]]; then
-  alias ls="gls --color"
-fi
---
+++ zsh/.zsh/eza.zsh
@@ -0,0 +1,3 @@
+if [[ -x "$(command -v eza)" ]]; then
+  alias ls="eza"
+fi
