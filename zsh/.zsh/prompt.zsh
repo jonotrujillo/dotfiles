cde0359ead8b0279de9aed836e7ae475a6d5b336
Jonathan Trujillo <git@jonathantrujillo.com>
2026-04-06 22:32:08 +0200

    zsh: add execution time to RPROMPT and % prompt character

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -14,6 +14,27 @@ function git_prompt_info() {
   echo "%F{242}at ${ref}${git_status}%f"
 }
 
+# Track command execution time
+function preexec() {
+  _cmd_start=$SECONDS
+}
+
+function precmd() {
+  if [[ -n $_cmd_start ]]; then
+    local elapsed=$(( SECONDS - _cmd_start ))
+    if (( elapsed >= 1 )); then
+      _cmd_time="${elapsed}s"
+    else
+      _cmd_time=""
+    fi
+    unset _cmd_start
+  else
+    _cmd_time=""
+  fi
+}
+
 PROMPT='
 %B%F{blue}%~%f%b $(git_prompt_info)
-'
+%% '
+
+RPROMPT='%F{242}${_cmd_time}%f'
