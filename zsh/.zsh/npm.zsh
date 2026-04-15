fb87a3286aada61a50bdc33e775bb6a48e5feda7
Jonathan Trujillo <git@jonathantrujillo.com>
2026-04-15 15:39:23 +0200

    zsh: simplify npm completion loading

--- zsh/.zsh/npm.zsh
+++ zsh/.zsh/npm.zsh
@@ -1,8 +1,3 @@
 if [[ -x "$(command -v npm)" ]]; then
-  # Cache npm completions to avoid spawning node on every shell start
-  npm_completion="${XDG_CACHE_HOME:-$HOME/.cache}/npm_completion.zsh"
-  if [[ ! -f "$npm_completion" ]]; then
-    npm completion > "$npm_completion"
-  fi
-  source "$npm_completion"
+  source <(npm completion)
 fi
